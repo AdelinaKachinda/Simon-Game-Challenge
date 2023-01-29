85e67dcb10ffcff04f1531be0e2f98ee085bd54c
AdelinaKachinda <adelinakachinda@users.noreplygithub.com>
2023-01-29 02:24:57 -0500

    adding comments

--- game.js
+++ game.js
@@ -1,30 +1,34 @@
-// arrays to store all available colors, the pattern in which the game goes and the users choices 
+// initializes the user's game level
 var level = 0;
+
+// arrays to store all available colors, the pattern in which the game goes and the users choices 
 var gamePattern = [];
 var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 
 $(".btn").click(function (){
-    // this function takes the users choice of color and stores it in an array
-    
+
+    // gets the colour id of the users choice and stores it in the array 
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     var last_index = userClickedPattern['length'] - 1
-    //
+
+    // calls this function to check if the user's answer was correct or wrong
     checkAnswer(last_index)
 
-    // calls playsound using the user's colour whatever the user picks
+    // calls playsound using the user's colour in order to play its respctive audio
     playsound(userChosenColour);
 
-    //adds animation using a css class that sets a background colour
+    //adds animation using a css class that sets a background animation on the buttons
     animatePress(userChosenColour);
     
 });
 
-// calling next sequence when a key press has been detected in order to tell the user to start level 1
+// This function calls next sequence and shows that the game has started
 function newGame(){
 
+    // This is used to detect a keyboard press in javascript
     var started_to_toggle = true
     $(document).keyup(function () {
 
@@ -71,19 +75,31 @@ function playsound(name){
 }
 
 
-// checking if the user is correct
+// checking if the user is correct and sets up  the game functionality 
 function checkAnswer(currentLevel){
+
+    // This checks if the user's selection was correct according to the the computer's selection
     if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
       if (userClickedPattern.length === gamePattern.length){
+
+        // Set timeout calls next sequence in intervals to ensure the game keeps going as long as the user is correct
         setTimeout(function(){
             nextSequence()
             userClickedPattern = []
         }, 900);
       }
+      
+      // This only runs when the user gets the sequence wrong
     }else{
+
+        // Plays the audio for a wrong choice
         var wrong = new Audio("sounds/wrong.mp3")
         wrong.play()
 
+        /* Adds a class to the body which causes a flash of a red background when the user is wrong.
+            It does this in a set interval of 70 miliseconds 
+        */
+        
         $("body").addClass("game-over")
         setTimeout(function(){
             $("h1").text("Game Over, press any key to restart")
@@ -91,6 +107,7 @@ function checkAnswer(currentLevel){
         }, 70);
 
        
+        // Restarts the game by calling the game initializer and reinitializes all the arrays to empty for a new game
         newGame()
         level = 0
         gamePattern = []
@@ -102,9 +119,8 @@ function checkAnswer(currentLevel){
 }
 
 
-
 function animatePress(currentColour){
-    // adds css class to color ids in order for a backgrouncolor 
+    // adds css class to color ids in order for a backgroun flash color on the buttons when they are pressed 
     
     $("#" + currentColour).addClass("pressed")
     setTimeout(function(){
